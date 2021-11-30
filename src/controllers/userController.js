dbb9fe6f1ed5910a7e65a6f1b0d4cf9f25c0296d
Gustavo <guuhfuuzer@gmail.com>
2021-11-30 17:55:41 -0300

    api2 atualizada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,14 +2,10 @@ const User = require("../models/User");
 
 exports.store = async (req, res, next) => {
   try{
-    const newUser = await User.create({
-      nome: 'gustavo',
-      email: 'guuhfuuzer@gmail.com',
-      password: '123456'
-    });
+    const newUser = await User.create();
     res.json(newUser);
   }catch(err){
     res.status(400);
-    res.json('deu erro')
+    res.json({errors: err.errors.map((err) => err.message)})
   }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -17,6 +17,9 @@ class User extends Model {
       email: {
         type: Sequelize.STRING,
         defaultValue: '',
+        unique: {
+          msg: 'E-mail já existe'
+        },
         validate: {
           isEmail: {
             msg: 'E-mail inválido'
